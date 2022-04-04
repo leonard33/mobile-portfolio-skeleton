5748fbdbe5e65d99b67a8cbda65ba1daf7be4db3
leonard33 <leonardkombo14@gmail.com>
2022-04-04 18:31:12 +0300

    Add javascript file and image

---
+++ script.js
@@ -0,0 +1,47 @@
+// query selectors
+const menuButton = document.querySelector('.menu-button')
+const closeButton = document.querySelector('.close');
+const menu= document.querySelector('#menu');
+const decoration =document.querySelector('.decoration');
+const personName = document.querySelector('.owner');
+const links = document.querySelectorAll('.menu-item');
+
+//event listener
+
+menuButton.addEventListener('click',openMenu);
+closeButton.addEventListener('click',closeMenu);
+
+//functions
+function openMenu() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+        menuButton.style.display="none";
+        personName.style.display = "none";
+        closeButton.style.display= "block"
+    } else {
+        menu.style.display = "none";       
+    }
+ 
+  }
+
+  function closeMenu() {
+      if(menu.style.display==="block"){
+          menu.style.display = "none";
+          closeButton.style.display="none";
+          menuButton.style.display = "block";
+          personName.style.display = "block";
+      }else {
+        menu.style.display = "block";     
+    }
+  }
+
+ links.forEach(element =>{
+     element.onclick = function(event){
+         if(event.target){
+             menu.style.display= "none";
+             menuButton.style.display = "block";
+             personName.style.display = "block";
+             closeButton.style.display="none";
+         }
+     }
+ })
